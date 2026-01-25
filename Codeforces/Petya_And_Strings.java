356ba4d51874446f00a59cceb70700d0ca7d4d22
Ahmed Abdullatif <ahmed3545ft65@gmail.com>
2026-01-25 23:08:55 +0200

    Add files via upload

---
+++ Codeforces/Petya_And_Strings.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Petya_And_Strings {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            String s1 = input.nextLine().toLowerCase();
+            String s2 = input.nextLine().toLowerCase();
+            int result = s1.compareTo(s2);
+
+            if (result < 0) {
+                System.out.println("-1");
+            } else if (result > 0) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        }
+    }
+}
